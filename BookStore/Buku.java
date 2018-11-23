36d090802f376f8fd7e8c5403d44e80c57a5677a
Faldi <13516098@std.stei.itb.ac.id>
2018-11-23 18:18:38 +0700

    merge sql retriever to Buku.java

--- BookStore/Buku.java
+++ BookStore/Buku.java
@@ -1,8 +1,10 @@
 import org.json.*;
 import java.net.*;
 import java.io.*;
+import java.sql.*;
 
 public class Buku {
+	
 	public static void main(String[] args) {
 		try{
 			URL url = new URL("https://www.googleapis.com/books/v1/volumes?q=" + "harry+potter");
@@ -29,7 +31,11 @@ public class Buku {
 			    String bookPublishedDate = arr.getJSONObject(i).getJSONObject("volumeInfo").getString("publishedDate");
 			    String bookImage = arr.getJSONObject(i).getJSONObject("volumeInfo").getJSONObject("imageLinks").getString("thumbnail");
 			    String bookDescription = fillBookDescription(arr.getJSONObject(i).getJSONObject("volumeInfo"));
-			    
+				String bookPrice = String.valueOf(getBookPrice(bookId));
+				if (bookPrice.equals("-1")) {
+					bookPrice = "Not For Sale";
+				}
+				
 			    System.out.println(i);
 			    System.out.println(bookId);
 			    System.out.println(bookTitle);
@@ -37,6 +43,7 @@ public class Buku {
 			    System.out.println(bookPublishedDate);
 				System.out.println(bookImage);	
 				System.out.println(bookDescription);
+				System.out.println(bookPrice);
 				System.out.println();
 			}
 		} catch	(Exception e) {
@@ -51,4 +58,38 @@ public class Buku {
 			return "";
 		}
 	}	
+
+	public static int getBookPrice(String bookId) {
+		try {
+            // create our mysql database connection
+            String myDriver = "org.gjt.mm.mysql.Driver";
+            String myUrl = "jdbc:mysql://localhost/bookstore?autoReconnect=true&useSSL=false";
+            Class.forName(myDriver);
+            Connection conn = DriverManager.getConnection(myUrl, "root", "1234");
+
+            // our SQL SELECT query.
+            // if you only need a few columns, specify them by name instead of using "*"
+			String query = "SELECT bookprice FROM book WHERE book.bookid='" + bookId + "'";
+
+            // create the java statement
+            Statement st = conn.createStatement();
+
+            // execute the query, and get a java resultset
+            ResultSet rs = st.executeQuery(query);
+
+            // iterate through the java resultset
+			
+			int bookPrice = -1;
+
+			while (rs.next()) {
+                bookPrice = rs.getInt("bookprice");
+            }
+			st.close();
+			return bookPrice;
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+			System.err.println(e.getMessage());
+			return -2;
+		}
+	}
 }
