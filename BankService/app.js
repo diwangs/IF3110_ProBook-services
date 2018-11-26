bf1270ee2c780fdc521664a3ba822bec62a00caf
daandeey <dandyarifrahman@gmail.com>
2018-11-26 12:56:00 +0700

    working on create_tx

--- BankService/app.js
+++ BankService/app.js
@@ -5,13 +5,13 @@ app.use(bodyParser.json()); // for parsing application/json
 
 // Database
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'bank'
-});
-connection.connect();
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : '',
+//   database : 'bank'
+// });
+// connection.connect();
 
 // REST
 // Validasi nasabah
@@ -26,6 +26,15 @@ app.get("/api/validate_customer/:id", (req, res) => {
 })
 
 app.post("/api/create_tx", (req, res) => {
+    connection.query('SELECT * FROM tx WHERE receiver_id='+req.body.receiver_id+', sender_id='+req.body.sender_id+', amount>='+req.body.amount, function (error, results, fields) {
+      if (error) throw error;
+      //If not found, return false
+      if (typeof results[0] == 'undefined') res.json({result:false})
+       // if found, update the balance on sender and receiver, return true
+      else {
+        res.json({result:true})
+      }
+    }); 
     // Untuk mengakses properti 'a', gunakan req.body.a
     console.log(req.body.a);
     // Teruskan hey Dandy dan Faldi
