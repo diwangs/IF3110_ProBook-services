c09e7c163fd3a967eb9e2b9f6a5b5f8210393314
Faldi <13516098@std.stei.itb.ac.id>
2018-11-30 19:41:50 +0700

    change probook database schema

--- ProBook/database/bookstore.sql
+++ ProBook/database/bookstore.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `book`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `book` (
-  `book_id` int(11) NOT NULL AUTO_INCREMENT,
+  `book_id` varchar(255) NOT NULL,
   `title` varchar(50) NOT NULL,
   `author` varchar(100) NOT NULL,
   `description` text,
@@ -38,7 +38,7 @@ CREATE TABLE `book` (
 
 LOCK TABLES `book` WRITE;
 /*!40000 ALTER TABLE `book` DISABLE KEYS */;
-INSERT INTO `book` VALUES (1,'Interaction Design','Jennifer Preece','Buku mata kuliah Interaksi Manusia Komputer','https://media.wiley.com/product_data/coverImage300/51/11190207/1119020751.jpg'),(4,'Artificial Intelligence','Stuart Russel','Buku mata kuliah Inteligensi Buatan','https://www.pearsonhighered.com/assets/bigcovers/0/1/3/3/0133001989.jpg');
+INSERT INTO `book` VALUES ('1','Interaction Design','Jennifer Preece','Buku mata kuliah Interaksi Manusia Komputer','https://media.wiley.com/product_data/coverImage300/51/11190207/1119020751.jpg'),('4','Artificial Intelligence','Stuart Russel','Buku mata kuliah Inteligensi Buatan','https://www.pearsonhighered.com/assets/bigcovers/0/1/3/3/0133001989.jpg');
 /*!40000 ALTER TABLE `book` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,14 +52,10 @@ DROP TABLE IF EXISTS `orders`;
 CREATE TABLE `orders` (
   `order_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
-  `book_id` int(11) NOT NULL,
+  `book_id` varchar(255) NOT NULL,
   `num_book` int(11) NOT NULL,
   `order_date` date NOT NULL,
-  PRIMARY KEY (`order_id`),
-  KEY `book_id` (`book_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`),
-  CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+  PRIMARY KEY (`order_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -69,7 +65,7 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-INSERT INTO `orders` VALUES (1,1,1,2,'2018-10-02'),(2,1,4,10,'2018-10-14'),(3,2,4,1,'2018-08-06');
+INSERT INTO `orders` VALUES (1,1,'1',2,'2018-10-02'),(2,1,'4',10,'2018-10-14'),(3,2,'4',1,'2018-08-06');
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -116,6 +112,7 @@ CREATE TABLE `user` (
   `addrs` varchar(100) DEFAULT NULL,
   `phone_num` varchar(30) DEFAULT NULL,
   `stat` tinyint(1) DEFAULT NULL,
+  `card_num` int(11) DEFAULT 1,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -126,7 +123,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'raflyalk','Muhammad Rafli Al Khadafi','1234','https://i.ytimg.com/vi/fpmua2YTu7M/maxresdefault.jpg','raflyalk@gmail.com','alamanda','081271371392',1),(2,'rifaldiu','Rifqi Rifaldi Utomo','4321','https://image-store.slidesharecdn.com/ce94d2fc-fe31-424d-8602-fc8af5368964-original.jpeg','13516056@std.stei.itb.ac.id','tubis','081231313211',1),(3,'aufaX45','Muhammad Aufa Helfiandri','kodokbeku','https://scontent-sin2-1.cdninstagram.com/vp/774930452d5dbcfb8b157edf678c2d15/5C6C1C92/t51.2885-15/e35/43779419_180566459493711_879659407431682033_n.jpg','13516008@std.stei.itb.ac.id','di hatimu','08911911911911',1);
+INSERT INTO `user` VALUES (1,'raflyalk','Muhammad Rafli Al Khadafi','1234','https://i.ytimg.com/vi/fpmua2YTu7M/maxresdefault.jpg','raflyalk@gmail.com','alamanda','081271371392',1,1),(2,'rifaldiu','Rifqi Rifaldi Utomo','4321','https://image-store.slidesharecdn.com/ce94d2fc-fe31-424d-8602-fc8af5368964-original.jpeg','13516056@std.stei.itb.ac.id','tubis','081231313211',1,1),(3,'aufaX45','Muhammad Aufa Helfiandri','kodokbeku','https://scontent-sin2-1.cdninstagram.com/vp/774930452d5dbcfb8b157edf678c2d15/5C6C1C92/t51.2885-15/e35/43779419_180566459493711_879659407431682033_n.jpg','13516008@std.stei.itb.ac.id','di hatimu','08911911911911',1,1);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -141,6 +138,9 @@ UNLOCK TABLES;
 
 -- Dump completed on 2018-10-25 13:47:06
 
+DROP TABLE IF EXISTS `active_token`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `active_token` (
   `token` varchar(32),
   `user_id` int(11),
